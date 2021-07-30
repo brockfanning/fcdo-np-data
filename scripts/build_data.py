6fde36279482aa3b7c3df7cb79c40c41cc36235a
Brock Fanning <brockfanning@gmail.com>
2021-07-30 11:50:30 -0400

    Quote fix

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,9 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_meta(meta):
+    for key in meta:
+        if meta[key] is not None and isinstance(meta[key], str):
+            meta[key] = meta[key].replace("'", "&#39;")
+    return meta
+
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
